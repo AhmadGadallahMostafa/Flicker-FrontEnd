35d6420a15607846ad0898ed8b50e8feaa1d815a
Senoussy <82662431+Senoussy@users.noreply.github.com>
2021-06-04 02:52:35 +0200

    Added Senoussy's work to the app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import {BrowserRouter,Route,Switch} from 'react-router-dom';
+import About from './About';
+import Help from './Help'
+import Getpro from './Getpro';
+import Proannual from './pro-annual';
+import Promonth from './pro-monthly';
+import Pro3month from './pro-3month';
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Flickr test, Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Switch>
+      <Route exact path="/about" component={About}/>
+      <Route exact path="/account/upgrade/pro" component={Getpro}/>
+      <Route exact path="/help" component={Help}/>
+      <Route exact path="/account/upgrade/pro/pro-annual" component={Proannual}/>
+      <Route exact path="/account/upgrade/pro/pro-monthly" component={Promonth}/>
+      <Route exact path="/account/upgrade/pro/pro-3month" component={Pro3month}/>
+    </Switch>
+    </BrowserRouter>
   );
 }
-
 export default App;
-
