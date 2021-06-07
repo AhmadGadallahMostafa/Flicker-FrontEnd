0c8f947ac554ce909c802156f22c733dc72fb138
Nour <42289144+NourZohry@users.noreply.github.com>
2021-06-07 16:57:19 +0200

    Update Navbar.js

--- src/components/AboutNavbar/Navbar.js
+++ src/components/AboutNavbar/Navbar.js
@@ -18,23 +18,23 @@ const Navbar = () => {
     return ( 
         <nav id="navbar-page" className="navbar">
             <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-            <a href="" className="nav_logo_click ani">
+            <a href="/#" className="nav_logo_click ani">
                 <img src={nav_logo} width='98' height='49' className="nav_flickr_logo ani" alt=""/>
             </a>
-            <a href="" className="signin-button nav-button">Sign Up</a>
-            <a href="" className="nav-button nav-button1 ani">Explore</a>
-            <a href="" className="nav-button nav-button2 ani">Prints</a>
-            <a href="" className="nav-button nav-button3 ani">Get Pro</a>
+            <a href="/#" className="signin-button nav-button">Sign Up</a>
+            <a href="/#" className="nav-button nav-button1 ani">Explore</a>
+            <a href="/#" className="nav-button nav-button2 ani">Prints</a>
+            <a href="/#" className="nav-button nav-button3 ani">Get Pro</a>
             <form action="" className="navbarsearch">
-                <a href="">
+                <a href="/#">
                     <img src={search} alt="" className="navbarsearch-icon"/>
                 </a>
                 <input type="text" placeholder="      Photos, people, or groups" className="navbarsearch-bar"></input>
             </form>
-            <a href="" className="nav-button4 ani">
+            <a href="/#" className="nav-button4 ani">
                 <img src={cloud} alt=""/>   
             </a>
-            <a href="" className="nav-button nav-button5 ani">Sign In</a>
+            <a href="/#" className="nav-button nav-button5 ani">Sign In</a>
 
         </nav>
      ); 
