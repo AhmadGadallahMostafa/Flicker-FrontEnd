9e3e997fe5897ef463dfc93b9e908925e86962be
Nour <42289144+NourZohry@users.noreply.github.com>
2021-06-07 14:16:05 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import GridText from './components/GridText';
 import PrintsButton from './components/PrintsButton';
 import Explore from './components/Explore';
 import FAQ from './components/FAQ';
+import Footer from './components/MinasFooter/Footer'
 
 function App() {
   return (
@@ -21,8 +22,8 @@ function App() {
       <Route exact path="/account/upgrade/pro/pro-annual" component={Proannual}/>
       <Route exact path="/account/upgrade/pro/pro-monthly" component={Promonth}/>
       <Route exact path="/account/upgrade/pro/pro-3month" component={Pro3month}/>
-      <Route path="/prints"> <PrintsButton /> <GridText /> <FAQ /> </Route>
-      <Route path="/explore"> <Explore /> </Route>
+      <Route path="/prints"> <PrintsButton /> <GridText /> <FAQ /> <Footer /> </Route>
+      <Route path="/explore"> <Explore /> <Footer /> </Route>
     </Switch>
     </BrowserRouter>
   );
