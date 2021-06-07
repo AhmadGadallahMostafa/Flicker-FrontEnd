9804e81f2589b5292d459422f05b43f76ce2483b
Nour <42289144+NourZohry@users.noreply.github.com>
2021-06-07 16:58:18 +0200

    Update Getpro-navbar.js

--- src/components/ProNavbar/Getpro-navbar.js
+++ src/components/ProNavbar/Getpro-navbar.js
@@ -17,11 +17,11 @@ const Navbar = () => {
         <nav className="getpro_navbar">
             <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
             <div className="getpro_nav_logo_click ">
-                <a href="" >
+                <a href="/#" >
                     <img src={nav_logo} width='98' height='49' className="getpro_nav_flickr_logo " alt=""/>
                 </a>
             </div>
-            <a href="" className="getpro_navbar_profile">
+            <a href="/#" className="getpro_navbar_profile">
                 <img src={profile} width='32' height='32' className="getpro_nav_profilepic" alt="" />
             </a>
             
