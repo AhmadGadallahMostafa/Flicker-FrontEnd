4ae9817fd555565944c5eff77bdbee88656174d1
Nour <42289144+NourZohry@users.noreply.github.com>
2021-06-08 20:02:35 +0200

    Update GalleryImages.js

--- src/components/GalleryImages.js
+++ src/components/GalleryImages.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 const GalleryImages = ({ galleryImages, setSelectedImage }) => {
 
 	const [isActive, setActive] = useState('');
-	
+	/*
 	const getSrc = (id) => {
 		fetch('https://www.thealphaflickr.xyz/api/photos/photo/' + id) // Photo - Returns photo itself
 			.then(res => {
@@ -19,12 +19,12 @@ const GalleryImages = ({ galleryImages, setSelectedImage }) => {
 				console.log(error);
 			  })
 	}
-
+*/
 	return (
 		<div className="image-gallery">
 			{galleryImages.map((image => (
 				<div className={isActive === image._id ? "grid-item active" : "grid-item"} onClick={() => { setActive(image._id); setSelectedImage(image._id) }} key={image._id}>
-					<img className={isActive === image._id ? "grid-item-image active" : "grid-item-image"} src={getSrc(image.url)} alt={image.title} />
+					<img className={isActive === image._id ? "grid-item-image active" : "grid-item-image"} src={"https://www.thealphaflickr.xyz/api/photos/photo/" + image._id} alt={image.title} />
 					<div className="grid-text">
 						<div className="grid-item-text" >{image.title}</div>
 						<div className="grid-item-subtext" >by {image.authorId /**Should be just author? */}</div>
