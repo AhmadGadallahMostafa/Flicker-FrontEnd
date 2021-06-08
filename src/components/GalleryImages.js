0c234c6d18bb18b896914d8e5477f4cbaf2eba86
Nour <42289144+NourZohry@users.noreply.github.com>
2021-06-08 22:39:40 +0200

    Update GalleryImages.js

--- src/components/GalleryImages.js
+++ src/components/GalleryImages.js
@@ -3,9 +3,9 @@ import { useState } from "react";
 const GalleryImages = ({ galleryImages, setSelectedImage }) => {
 
 	const [isActive, setActive] = useState('');
-	
+	/*
 	const getSrc = (id) => {
-		fetch('https://www.thealphaflickr.xyz/api/photos/photo/' + id) // Photo - Returns photo itself
+		fetch() // Photo - Returns photo itself
 			.then(res => {
 				return res.json();
 			})
@@ -18,12 +18,12 @@ const GalleryImages = ({ galleryImages, setSelectedImage }) => {
 				console.log(error);
 			  })
 	}
-
+*/
 	return (
 		<div className="image-gallery">
 			{galleryImages.map((image => (
 				<div className={isActive === image._id ? "grid-item active" : "grid-item"} onClick={() => { setActive(image._id); setSelectedImage(image._id) }} key={image._id}>
-					<img className={isActive === image._id ? "grid-item-image active" : "grid-item-image"} src={getSrc(image._id)} alt={image.title} />
+					<img className={isActive === image._id ? "grid-item-image active" : "grid-item-image"} src={'https://www.thealphaflickr.xyz/api/photos/photo/' + image._id} alt={image.title} />
 					<div className="grid-text">
 						<div className="grid-item-text" >{image.title}</div>
 						<div className="grid-item-subtext" >by {image.authorId /**Should be just author? */}</div>
@@ -34,4 +34,4 @@ const GalleryImages = ({ galleryImages, setSelectedImage }) => {
 	);
 }
 
-export default GalleryImages
+export default GalleryImages;
